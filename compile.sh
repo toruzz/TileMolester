cecb2f46d70dd5c421be7637d7ec6528719b0615
Z3r0CooL <joshman523@gmail.com>
2022-05-03 16:13:06 -0400

    Changelog: - Removed line that would break shell script

--- compile.sh
+++ compile.sh
@@ -2,6 +2,4 @@ rm -rf ./build/*
 javac -sourcepath src -d ./build src/TileMolester.java
 cp -R resources/icons/ build/production/TileMolester/tm/
 cp resources/splash.gif build/production/TileMolester/tm/splash.gif
-cp  -R resources/languages/ ./
 jar cvfm TileMolester.jar META-INF/MANIFEST.MF -C build/ .
-rm -rf ./languages
